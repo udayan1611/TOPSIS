fe42ae62a2c2b0f6da9dbce9b6894f3c75c4b187
udayan1611 <157144796+udayan1611@users.noreply.github.com>
2024-07-11 12:24:44 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -11,4 +11,24 @@ import { TopsisTableComponent } from "./topsis-table/topsis-table.component";
 })
 export class AppComponent {
   title = 'topsis-app';
+  constructor(private topsisService: TopsisService) { }
+
+  calculate() {
+    const requestBody = {
+      alternatives: ['Alternative1', 'Alternative2', 'Alternative3'],
+      criteria: ['Criteria1', 'Criteria2', 'Criteria3'],
+      criteriaValues: [
+          [1.0, 2.0, 3.0],
+          [4.0, 5.0, 6.0],
+          [7.0, 8.0, 9.0]
+      ],
+      weights: [0.5, 0.3, 0.2],
+      benefitCriteria: [true, false, true]
+    };
+
+    this.topsisService.calculateTopsis(requestBody).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
+
